7026a9147ccdfbf3711e0a92af0d22443e221a13
amarildo <amarildo.rajta@gmail.com>
2023-04-03 02:21:40 +0200

    first change. no progress

--- L1/main.cpp
+++ L1/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <set>
+#include <fstream>
+#include "reader.h"
+
+using namespace std;
+
+void dfs(const vector<vector<int>>& adj_list, vector<bool>& visited, int node) {
+    visited[node] = true;
+
+    for (int i = 0; i < adj_list[node].size(); ++i) {
+        int neighbor = adj_list[node][i];
+        if (!visited[neighbor]) {
+            dfs(adj_list, visited, neighbor);
+        }
+    }
+}
+
+void check_validity(const adjacency_list_t& adj_list) {
+    int num_nodes = 0;
+
+    // Calculate the number of nodes in the graph
+    for (const auto& e : adj_list.second) {
+        num_nodes = max(num_nodes, max(e.n1, e.n2));
+    }
+    num_nodes++; // Increment to get the actual number of nodes
+
+    // Convert edge list to adjacency list
+    vector<vector<int>> adj(num_nodes);
+    for (auto& e : adj_list.second) {
+        adj[e.n1].push_back(e.n2);
+        adj[e.n2].push_back(e.n1);
+    }
+
+    vector<bool> visited(num_nodes, false);
+
+    // Run DFS on first node
+    dfs(adj, visited, 0);
+
+    // Check if all nodes were visited
+    bool valid = true;
+    for (int i = 0; i < num_nodes; ++i) {
+        if (!visited[i]) {
+            cout << "Graph is not connected. Node " << i << " is not reachable from Node 0." << endl;
+            valid = false;
+        }
+    }
+
+    if (valid) {
+        cout << "Graph is valid." << endl;
+    }
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Parse the input file and get adjacency list
+    adjacency_list_t adj_list = parse_file("../edges.txt");
+
+
+    // Check if the graph is valid
+    check_validity(adj_list);
+
     return 0;
 }
---
+++ L1/reader.cpp
@@ -0,0 +1,71 @@
+//
+// Created by amarildo on 2023-04-02.
+//
+
+#include "reader.h"
+
+#include<fstream>
+#include<string>
+#include<map>
+
+
+enum token{
+    COMMENT, // The line is a comment
+    META, // The line is metadata
+    EDGE, // The line describes an edge
+    END_OF_FILE // End of the file has been reached
+};
+
+// Determine the type of the next line in the input stream
+token get_line_type(std::istream& is){
+    switch(is.peek()){
+        case std::istream::traits_type::eof(): return END_OF_FILE;
+        case '#': return COMMENT;
+        case 'M': return META;
+    };
+    return EDGE;
+}
+
+// A map to store metadata
+meta_t meta;
+
+// Read an edge from the input stream
+edge read_edge(std::istream& is){
+    edge e;
+    is >> e.n1 >> e.n2 >> e.weight;
+    std::getline(is, e.description);
+    return e;
+}
+
+// Read metadata from the input stream and store it in the meta map
+void read_meta(std::istream& is){
+    char discard;
+    node_id_t vertex_id;
+    std::string name;
+    is >> discard >> vertex_id;
+    std::getline(is, name);
+    meta[vertex_id] = name;
+}
+
+// Parse a file and return an adjacency list
+adjacency_list_t parse_file(std::string filename){
+    std::ifstream in(filename);
+    token l;
+    edge_list_t edge_list;
+    while((l = get_line_type(in)) != END_OF_FILE){
+        edge e;
+        switch(l){
+            case token::EDGE:
+                e = read_edge(in);
+                edge_list.push_back(e);
+                break;
+            case token::META:
+                read_meta(in);
+                break;
+            default:
+                std::string comment;
+                std::getline(in, comment);
+        }
+    }
+    return adjacency_list_t{meta, edge_list};
+}
