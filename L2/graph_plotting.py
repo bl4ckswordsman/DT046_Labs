e70e3fb646fbbf9d5b87e494067722528a5927e6
amarildo <amarildo.rajta@gmail.com>
2023-04-23 18:36:38 +0200

    plot title and labels fixed. added prefix for filename dir

--- L2/graph_plotting.py
+++ L2/graph_plotting.py
@@ -7,13 +7,20 @@ from scipy.optimize import curve_fit
 # import matplotlib
 # matplotlib.use('Qt5Agg')
 
+
+# Get the point label from the plot title of the format "XXXXX_sort_LABEL"
+def get_point_lab(plot_title):
+    return plot_title[plot_title.index("_sort_") + len("_sort_"):]
+
+
 def plot_data(file_name):
     # Read in the data file using pandas
     df = pd.read_csv(file_name, delim_whitespace=True, skiprows=1)
 
     # Set the plot title
     with open(file_name) as f:
-        plot_title = f.readline().strip()
+        plot_title_temp = f.readline().strip()
+    plot_title = plot_title_temp[:plot_title_temp.index("sort") + len("sort")]
 
     # Set the first column as the index
     df = df.set_index('N')
@@ -29,8 +36,10 @@ def plot_data(file_name):
     # Create the plot
     x_new = np.linspace(n[0], n[-1], 100)
     y_new = np.polyval(popt, x_new)
-    plt.errorbar(n, mean_time_ms, yerr=stdev_ms, fmt='o', capsize=5, label='Data')  # 'o-' for line
-    plt.plot(x_new, y_new, '-', label='Fit')
+
+    point_lab = get_point_lab(plot_title_temp)
+    plt.errorbar(n, mean_time_ms, yerr=stdev_ms, fmt='o', capsize=5, label=point_lab)  # 'o-' for line
+    plt.plot(x_new, y_new, '-', label=point_lab)     # , label='Fit'
 
     # Set the title and axis labels
     plt.title(plot_title)
@@ -39,7 +48,14 @@ def plot_data(file_name):
     plt.legend()
 
 
-plot_data('cmake-build-release/table.txt')
+prefix = 'cmake-build-release/'
+
+plot_data(prefix + 'insert_sort_const.txt')
+plot_data(prefix + 'insert_sort_rand.txt')
+plot_data(prefix + 'insert_sort_monot_incr.txt')
+plot_data(prefix + 'insert_sort_monot_decr.txt')
+
+
 
 # Display the plot
 plt.show()
