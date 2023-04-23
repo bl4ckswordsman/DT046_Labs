0ab5b5e0a86c2664c26fb201e2daf3e6d20d9346
amarildo <amarildo.rajta@gmail.com>
2023-04-23 15:03:15 +0200

    new tests

--- L2/main.cpp
+++ L2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include "comp_sorting.h"
 #include "data_gen.h"
 #include "time_measurement.h"
@@ -14,9 +15,12 @@ double calculate_stdev(std::vector<double> data, double mean) {
 
 int main() {
     std::ofstream out_file("table.txt");
-    out_file << "N\tT[ms]\tStdev[ms]\tSamples\n";
+    //out_file << "N\tT[ms]\tStdev[ms]\tSamples\n";
+    out_file << "Insertion sort\n";
+    out_file << std::left << std::setw(15) << "N" << std::setw(15) << "T[ms]" << std::setw(15) << "Stdev[ms]"
+            << std::setw(15) << "Samples" << "\n";
 
-    std::vector<int> ns{10, 100, 1000, 10000, 100000};
+    std::vector<int> ns{2000, 8000, 12000, 50000, 100000};
 
     for (auto& n : ns) {
         std::vector<double> times(5);
@@ -37,7 +41,9 @@ int main() {
         double stdev_ms = calculate_stdev(times, mean_time_ms);
 
 
-        out_file << n << "\t" << mean_time_ms << "\t" << stdev_ms << "\t" << times.size() << "\n";
+        //out_file << n << "\t" << mean_time_ms << "\t" << stdev_ms << "\t" << times.size() << "\n";
+        out_file << std::left << std::setw(15) << n << std::setw(15) << mean_time_ms << std::setw(15)
+                << stdev_ms << std::setw(15) << times.size() << "\n";
 
         // print results
         std::cout << "n: " << n << ", mean time: " << mean_time_ms << ", stdev: " << stdev_ms << "\n";
