5b03b14bdd08e14624a78ad7df70972034d874f5
amarildo <amarildo.rajta@gmail.com>
2023-04-30 13:14:06 +0200

    final tests

--- L2/main.cpp
+++ L2/main.cpp
@@ -9,42 +9,48 @@ int main() {
     const std::string prfx = "../Test_results/"; // Prefix for the output files
 
         // Insertion sort
-    /*measure_sort_perf("insert_sort_rand.txt", insertion_sort, rand_gen_in_vect);
-    measure_sort_perf("insert_sort_const.txt", insertion_sort, const_gen_in_vect);
-    measure_sort_perf("insert_sort_monot_incr.txt", insertion_sort, monot_incr_gen_in_vect);
-    measure_sort_perf("insert_sort_monot_decr.txt", insertion_sort, monot_decr_gen_in_vect);*/
+    /*measure_sort_perf(prfx + "insert_sort_rand.txt", insertion_sort, rand_gen_in_vect);
+    measure_sort_perf(prfx + "insert_sort_const.txt", insertion_sort, const_gen_in_vect);
+    measure_sort_perf(prfx + "insert_sort_monot_incr.txt", insertion_sort, monot_incr_gen_in_vect);
+    measure_sort_perf(prfx + "insert_sort_monot_decr.txt", insertion_sort, monot_decr_gen_in_vect);*/
 
         // Selection sort
-    /*measure_sort_perf("select_sort_rand.txt", selection_sort, rand_gen_in_vect);
-    measure_sort_perf("select_sort_const.txt", selection_sort, const_gen_in_vect);
-    measure_sort_perf("select_sort_monot_incr.txt", selection_sort, monot_incr_gen_in_vect);
-    measure_sort_perf("select_sort_monot_decr.txt", selection_sort, monot_decr_gen_in_vect);*/
+    /*measure_sort_perf(prfx + "select_sort_rand.txt", selection_sort, rand_gen_in_vect);
+    measure_sort_perf(prfx + "select_sort_const.txt", selection_sort, const_gen_in_vect);
+    measure_sort_perf(prfx + "select_sort_monot_incr.txt", selection_sort, monot_incr_gen_in_vect);
+    measure_sort_perf(prfx + "select_sort_monot_decr.txt", selection_sort, monot_decr_gen_in_vect);*/
 
     // Hoare right-pivot quick sort
-    measure_sort_perf(prfx + "right_pivot_quick_sort_rand.txt", r_pivot_quick_sort_test,
+/*    measure_sort_perf(prfx + "right_pivot_quick_sort_rand.txt", r_pivot_quick_sort_test,
                       rand_gen_in_vect);
     measure_sort_perf(prfx + "right_pivot_quick_sort_const.txt", r_pivot_quick_sort_test,
                       const_gen_in_vect);
     measure_sort_perf(prfx + "right_pivot_quick_sort_monot_incr.txt", r_pivot_quick_sort_test,
                       monot_incr_gen_in_vect);
     measure_sort_perf(prfx + "right_pivot_quick_sort_monot_decr.txt", r_pivot_quick_sort_test,
-                      monot_decr_gen_in_vect);
+                      monot_decr_gen_in_vect);*/
 
     // Hoare median_of_3-of-three quick sort
-    /*measure_sort_perf("medof3_quick_sort_rand.txt", med3_pivot_quick_sort_test,
+/*    measure_sort_perf(prfx + "medof3_quick_sort_rand.txt", med3_quick_sort_test,
                       rand_gen_in_vect);
-    measure_sort_perf("medof3_quick_sort_const.txt", med3_pivot_quick_sort_test,
-                        const_gen_in_vect);
-    measure_sort_perf("medof3_quick_sort_monot_incr.txt", med3_pivot_quick_sort_test,
-                        monot_incr_gen_in_vect);
-    measure_sort_perf("medof3_quick_sort_monot_decr.txt", med3_pivot_quick_sort_test,
-                        monot_decr_gen_in_vect);*/
+    measure_sort_perf(prfx + "medof3_quick_sort_const.txt", med3_quick_sort_test,
+                      const_gen_in_vect);
+    measure_sort_perf(prfx + "medof3_quick_sort_monot_incr.txt", med3_quick_sort_test,
+                      monot_incr_gen_in_vect);
+    measure_sort_perf(prfx + "medof3_quick_sort_monot_decr.txt", med3_quick_sort_test,
+                      monot_decr_gen_in_vect);*/
 
     // std::sort
-    /*measure_std_sort_perf("std_sort_rand.txt", rand_gen_in_vect);
-    measure_std_sort_perf("std_sort_const.txt", const_gen_in_vect);
-    measure_std_sort_perf("std_sort_monot_incr.txt", monot_incr_gen_in_vect);
-    measure_std_sort_perf("std_sort_monot_decr.txt", monot_decr_gen_in_vect);*/
+/*    measure_sort_perf(prfx + "std_sort_rand.txt", std_sort_test,
+                      rand_gen_in_vect);
+    measure_sort_perf(prfx + "std_sort_const.txt", std_sort_test,
+                        const_gen_in_vect);
+    measure_sort_perf(prfx + "std_sort_monot_incr.txt", std_sort_test,
+                        monot_incr_gen_in_vect);
+    measure_sort_perf(prfx + "std_sort_monot_decr.txt", std_sort_test,
+                        monot_decr_gen_in_vect);*/
+
+
 
 
     /*Algorithm                     Random      Constant        Increasing      Decreasing
@@ -54,15 +60,6 @@ int main() {
     Quicksort (Median of 3 Pivot)   O(n*log(n)) O(n*log(n))     O(n*log(n))     O(n*log(n))
     std::sort                       O(n*log(n)) O(n*log(n))     O(n*log(n))     O(n*log(n)) */
 
-
-/*    measure_sort_perf("medof3_quick_sort_rand.txt", quicksort_test,
-                      rand_gen_in_vect);
-    measure_sort_perf("medof3_quick_sort_const.txt", quicksort_test,
-                      const_gen_in_vect);
-    measure_sort_perf("medof3_quick_sort_monot_incr.txt", quicksort_test,
-                        monot_incr_gen_in_vect);
-    measure_sort_perf("medof3_quick_sort_monot_decr.txt", quicksort_test,
-                        monot_decr_gen_in_vect);*/
 
 
 
