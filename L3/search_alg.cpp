2a10b29684ecc0067dc462ac84a0a2f46507f748
amarildo <amarildo.rajta@gmail.com>
2023-05-05 20:06:21 +0200

    new TreeNode struct with what seems a working bin. search method

--- L3/search_alg.cpp
+++ L3/search_alg.cpp
@@ -12,19 +12,62 @@ int linear_search(std::vector<bool> &vect, int target) {
     return -1;
 }
 
-int binary_search(std::vector<bool> &vect, int target) {
-    int low = 0;
-    int high = vect.size() - 1;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (vect[mid] == target)
-            return mid;
-        else if (vect[mid] < target)
-            low = mid + 1;
-        else
-            high = mid - 1;
+int TreeNode::binary_search(TreeNode *root, int target) {
+    TreeNode* current = root;
+    while (current != nullptr) {
+        if (target == current->data) {
+            return current->data;
+        } else if (target < current->data) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
     }
     return -1;
 }
 
+void TreeNode::insert(int &i) {
+    if (i < this->data) {
+        if (this->left == nullptr) {
+            this->left = new TreeNode();
+            this->left->data = i;
+            this->left->parent = this;
+            this->left->left = nullptr;
+            this->left->right = nullptr;
+        } else {
+            this->left->insert(i);
+        }
+    } else {
+        if (this->right == nullptr) {
+            this->right = new TreeNode();
+            this->right->data = i;
+            this->right->parent = this;
+            this->right->left = nullptr;
+            this->right->right = nullptr;
+        } else {
+            this->right->insert(i);
+        }
+    }
+
+}
+
+TreeNode::TreeNode(std::vector<int> &vect) {
+    if (vect.empty()) {
+        this->data = 0;
+        this->parent = nullptr;
+        this->left = nullptr;
+        this->right = nullptr;
+        return;
+    }
+
+    this->data = vect[0];
+    this->parent = nullptr;
+    this->left = nullptr;
+    this->right = nullptr;
+
+    for (unsigned int i = 1; i < vect.size(); i++) {
+        this->insert(vect[i]);
+    }
+
+}
--- L3/search_alg.h
+++ L3/search_alg.h
@@ -12,7 +12,31 @@
 
 int linear_search(std::vector<bool>& vect, int target);
 
-int binary_search(std::vector<bool>& vect, int target);
+struct TreeNode {
+    int data;
+    TreeNode* parent;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode() {
+        this->data = 0;
+        this->parent = nullptr;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+
+    ~TreeNode() {
+        delete this->left;
+        delete this->right;
+    }
+
+    void insert(int &i);
+
+    TreeNode(std::vector<int>& vect);
+
+    int binary_search(TreeNode* root, int target);
+};
+
 
 
 
