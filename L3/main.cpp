2099fd30f6c02647359dd508952fb367917b64ce
amarildo <amarildo.rajta@gmail.com>
2023-05-09 16:34:10 +0200

    new benchmarks

--- L3/main.cpp
+++ L3/main.cpp
@@ -19,18 +19,21 @@ int main() {
     //measure_search_perf(prfx + "linear_search.txt", linear_search, gen_primes);
     //measure_search_perf("binary_search.csv", binary_search, gen_primes);
 
-    std::vector<std::vector<int>> data(5);
-    gen_rand(1000, data[0]);
-    gen_rand(5'000, data[1]);
-    gen_rand(10'000, data[2]);
-    gen_rand(20'000, data[3]);
-    gen_rand(50'000, data[4]);
+    std::vector<int> samples = {1'000, 2'000, 3'000, 4'000, 5'000, 10'000, 12'000, 14'000, 16'000,
+                                18'000, 20'000};
+    std::vector<std::vector<int>> data(samples.size());
+    for (auto i = 0; i < samples.size(); i++) {
+        gen_rand(samples[i], data[i]);
+    }
+
 
     measure_search_perf3(prfx + "linear_search.txt", linear_search_int, data);
     measure_search_perf3(prfx + "binary_search.txt", binary_search_int, data);
 
 
 
+/*  Linear Search      Binary Search       Binary Tree Search      Hash Table Search
+    O(N)               O(logN)             O(logN)                 O(1)                    */
 
     return 0;
 }
